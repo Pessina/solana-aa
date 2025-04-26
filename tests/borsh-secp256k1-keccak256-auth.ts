8b98b9015a69f6b736579312c86bc8c1bebfd827
Pessina <fs.pessina@gmail.com>
2025-04-27 01:31:25 +0400

    Include test for all types of transaction

--- tests/borsh-secp256k1-keccak256-auth.ts
+++ tests/borsh-secp256k1-keccak256-auth.ts
@@ -1,7 +1,12 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { signWithEthereum } from "../utils/secp256k1-signer";
-import { borshUtils, Transaction } from "../utils/borsh";
+import {
+  borshUtils,
+  Identity,
+  IdentityWithPermissions,
+  Transaction,
+} from "../utils/borsh";
 import { SolanaAa } from "../target/types/solana_aa";
 import { confirmTransaction, getTxInfo } from "../utils/solana";
 import {
@@ -9,40 +14,24 @@ import {
   ethereumAddressToBytes,
   createSecp256k1VerificationInstruction,
 } from "../utils/ethereum";
-import { keccak256 } from "viem";
+import { keccak256, toBytes } from "viem";
 import { expect } from "chai";
 import * as _ from "lodash";
 import { normalize } from "../utils/utils";
 
 const PRIVATE_KEY =
-  "0x4646464646464646464646464646464646464646464646464646464646464646" as const;
+  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80" as const;
+const ETH_ADDRESS = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 
 describe.only("Ethereum Signature Verification", () => {
   const provider = anchor.getProvider() as anchor.AnchorProvider;
   const program = anchor.workspace.solanaAa as Program<SolanaAa>;
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  it("should validate Ethereum signature correctly", async () => {
-    const transaction: Transaction = {
-      account_id: 1n,
-      nonce: 135n,
-      action: {
-        AddIdentity: {
-          identity: {
-            Wallet: {
-              Ethereum: new Uint8Array([
-                0x04, 0xab, 0x3c, 0xb2, 0x89, 0x73, 0x44, 0xaa, 0x3f, 0x6f,
-                0xfa, 0xac, 0x94, 0xe4, 0x77, 0xae, 0xac, 0x17, 0x0b, 0x92,
-              ]),
-            },
-          },
-          permissions: {
-            enable_act_as: true,
-          },
-        },
-      },
-    };
-
+  async function verifyEthereumSignature(transaction: Transaction): Promise<{
+    deserializedTransaction: Transaction;
+    ethAddressHex: string;
+  }> {
     const serializedMessage = Buffer.from(
       borshUtils.serialize.transaction(transaction)
     );
@@ -76,7 +65,6 @@ describe.only("Ethereum Signature Verification", () => {
     await confirmTransaction(provider.connection, txSignature);
 
     const result = await getTxInfo({ txSignature });
-
     const returnData = result?.meta?.returnData?.data[0];
     const decodedData = Buffer.from(returnData, "base64");
 
@@ -89,6 +77,101 @@ describe.only("Ethereum Signature Verification", () => {
 
     const deserializedTransaction =
       borshUtils.deserialize.transaction(transactionData);
+
+    return { deserializedTransaction, ethAddressHex };
+  }
+
+  it("should validate Ethereum signature correctly", async () => {
+    const transaction: Transaction = {
+      account_id: 1n,
+      nonce: 135n,
+      action: {
+        AddIdentity: {
+          identity: {
+            Wallet: {
+              Ethereum: toBytes(ETH_ADDRESS),
+            },
+          },
+          permissions: {
+            enable_act_as: true,
+          },
+        },
+      },
+    };
+
+    const { deserializedTransaction } = await verifyEthereumSignature(
+      transaction
+    );
+
+    const normalizedOriginal = normalize(transaction);
+    const normalizedDeserialized = normalize(deserializedTransaction);
+
+    expect(normalizedDeserialized).to.deep.equal(normalizedOriginal);
+  });
+
+  it("should verify a transaction with RemoveAccount action", async () => {
+    const transaction: Transaction = {
+      account_id: BigInt(1),
+      nonce: BigInt(Date.now()),
+      action: { RemoveAccount: {} },
+    };
+
+    const { deserializedTransaction } = await verifyEthereumSignature(
+      transaction
+    );
+
+    const normalizedOriginal = normalize(transaction);
+    const normalizedDeserialized = normalize(deserializedTransaction);
+
+    expect(normalizedDeserialized).to.deep.equal(normalizedOriginal);
+  });
+
+  it("should verify a transaction with RemoveIdentity action", async () => {
+    const identity: Identity = {
+      Wallet: {
+        Ethereum: toBytes(ETH_ADDRESS),
+      },
+    };
+
+    const transaction: Transaction = {
+      account_id: BigInt(1),
+      nonce: BigInt(Date.now()),
+      action: { RemoveIdentity: identity },
+    };
+
+    const { deserializedTransaction } = await verifyEthereumSignature(
+      transaction
+    );
+
+    const normalizedOriginal = normalize(transaction);
+    const normalizedDeserialized = normalize(deserializedTransaction);
+
+    expect(normalizedDeserialized).to.deep.equal(normalizedOriginal);
+  });
+
+  it("should verify a transaction with AddIdentity action", async () => {
+    const identity: Identity = {
+      Wallet: {
+        Ethereum: toBytes(ETH_ADDRESS),
+      },
+    };
+
+    const identityWithPermissions: IdentityWithPermissions = {
+      identity,
+      permissions: {
+        enable_act_as: true,
+      },
+    };
+
+    const transaction: Transaction = {
+      account_id: BigInt(1),
+      nonce: BigInt(Date.now()),
+      action: { AddIdentity: identityWithPermissions },
+    };
+
+    const { deserializedTransaction } = await verifyEthereumSignature(
+      transaction
+    );
 
     const normalizedOriginal = normalize(transaction);
     const normalizedDeserialized = normalize(deserializedTransaction);
