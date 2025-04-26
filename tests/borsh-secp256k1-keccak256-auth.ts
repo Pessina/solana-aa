15a76d4efa9852ec4596c1285dbf69d7dc7660fb
Pessina <fs.pessina@gmail.com>
2025-04-27 01:31:47 +0400

    Remove debug code

--- tests/borsh-secp256k1-keccak256-auth.ts
+++ tests/borsh-secp256k1-keccak256-auth.ts
@@ -23,7 +23,7 @@ const PRIVATE_KEY =
   "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80" as const;
 const ETH_ADDRESS = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 
-describe.only("Ethereum Signature Verification", () => {
+describe("Ethereum Signature Verification", () => {
   const provider = anchor.getProvider() as anchor.AnchorProvider;
   const program = anchor.workspace.solanaAa as Program<SolanaAa>;
   anchor.setProvider(anchor.AnchorProvider.env());
